c15dbacd7c616af628e0a7ea6cc5617644fd8540
amerdommi241-oss <amerdommi241-oss@example.com>
2026-08-17 17:00:26 +0800

    feat: 开启 inlineStylesheets 模式，将 CSS 全部直接写入 HTML <style> 标签，解决无样式渲染问题

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,7 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   site: 'https://clash-top.com',
   compressHTML: true,
+  build: {
+    inlineStylesheets: 'always',
+  },
 });
